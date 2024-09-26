60bd5892f117ff016646b54ae41db56a586e5a61
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-09-26 17:22:47 +0300

    son durum

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,11 @@ import PageContainer from './container/PageContainer'
 import Header from './components/Header'
 import RouterConfig from './config/RouterConfig'
 import Loading from './components/Loading'
-import { useDispatch, useSelector } from 'react-redux'
-import { calculateBasket, setDrawer, removeFromBasket, setBasket} from './redux/slices/basketSlice'
-import { useEffect } from 'react'
-import { ToastContainer, toast } from 'react-toastify'
+import { ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom'
 
 function App() {
-  const { products, drawer, totalAmount } = useSelector((store) => store.basket);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    dispatch(calculateBasket());
-  }, [dispatch]);
-
-  const handleRemove = (product) => {
-    dispatch(removeFromBasket(product));  
-    dispatch(calculateBasket()); 
-    
-    toast.error(`${product.title} sepetten kaldırıldı!`, {
-      position: "top-right",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-    });
-  };
-  const handleCompleteOrder = () => {
-   
-    localStorage.clear(); 
-    dispatch(setBasket([])); 
-    dispatch(setDrawer()); 
-    dispatch(calculateBasket());
-    toast.success("Sepetiniz tamamlandı!", {
-      position: "top-right",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-    });
-    navigate('/'); 
-  };
-
+ 
   return (
     <div>
       <ToastContainer />
--- src/components/BasketPage.jsx
+++ src/components/BasketPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { calculateBasket, removeFromBasket, setBasket } from '../redux/slices/basketSlice';
 import { toast } from 'react-toastify';
